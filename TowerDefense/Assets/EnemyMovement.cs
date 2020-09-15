be5974da506cb76d4d83d9d6a3d8655f12270ac1
Elvis <letlive419@gmail.com>
2020-09-15 17:05:28 -0400

    WayPoint

---
+++ TowerDefense/Assets/EnemyMovement.cs
@@ -0,0 +1,21 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+
+public class EnemyMovement : MonoBehaviour
+{
+    [SerializeField] List <WayPoint> path;
+    // Start is called before the first frame update
+    void Start()
+    {
+        foreach (WayPoint wayPoint in path)
+            print(wayPoint);
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        
+    }
+}
---
+++ TowerDefense/Assets/editorsnap.cs
@@ -0,0 +1,32 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+[ExecuteInEditMode]
+[SelectionBase]
+public class editorsnap : MonoBehaviour
+{
+    [SerializeField] [Range(1f, 20f)] float gridSize = 10f;
+
+    TextMesh textMesh;
+    // Update is called once per frame
+    void Update()
+    {
+        textMesh = GetComponentInChildren<TextMesh>();
+        
+
+        Vector3 snapPos;
+        snapPos.x = Mathf.RoundToInt(transform.position.x / gridSize) * gridSize;
+        snapPos.z = Mathf.RoundToInt(transform.position.z / gridSize) * gridSize;
+
+        transform.position = new Vector3(snapPos.x, 0f, snapPos.z);
+
+        string labelCube = snapPos.x / gridSize + "," + snapPos.z / gridSize;
+
+        textMesh.text = labelCube;
+
+        gameObject.name = labelCube;
+
+
+    }
+}
