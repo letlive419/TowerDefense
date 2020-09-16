8b9f65d22be9091f876e64e561f0936caa8f7f96
Elvis <letlive419@gmail.com>
2020-09-16 12:17:40 -0400

    fixed game

--- TowerDefense/Assets/EnemyMovement.cs
+++ TowerDefense/Assets/EnemyMovement.cs
@@ -1,21 +1,28 @@
-﻿using System.Collections;
+﻿using System;
+using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
 
 
 public class EnemyMovement : MonoBehaviour
 {
-    [SerializeField] List <WayPoint> path;
+    [SerializeField] List<WayPoint> path;
     // Start is called before the first frame update
     void Start()
     {
-        foreach (WayPoint wayPoint in path)
-            print(wayPoint);
+        StartCoroutine(Movement());
     }
 
-    // Update is called once per frame
-    void Update()
+    IEnumerator Movement()
     {
-        
+        foreach (WayPoint wayPoint in path)
+        { 
+            transform.position = wayPoint.transform.position;
+            print(wayPoint);
+
+        yield return new WaitForSeconds(1f);
+        }
+
     }
+   
 }
