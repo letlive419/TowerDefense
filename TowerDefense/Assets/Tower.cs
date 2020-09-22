d67e2961d38dfd7a1eb22f797430acbb27a59d38
Elvis <letlive419@gmail.com>
2020-09-22 12:37:09 -0400

    update tower shooting movement

--- TowerDefense/Assets/Tower.cs
+++ TowerDefense/Assets/Tower.cs
@@ -6,12 +6,15 @@ using UnityEngine;
 public class Tower : MonoBehaviour
 {
     [SerializeField] Transform objectToPan;
-    [SerializeField] Transform targetEnemy;
     [SerializeField] ParticleSystem projectileParticle;
     [SerializeField] float attackRange = 10f;
-    // Update is called once per frame
+
+    Transform targetEnemy;
+    
     void Update()
     {
+        setEnemy();
+
         if (targetEnemy)
         {
             lookAtEnemy();
@@ -22,6 +25,36 @@ public class Tower : MonoBehaviour
             Shoot(false);
         }
             
+    }
+
+    private void setEnemy()
+    {
+        var sceneEnemies = FindObjectsOfType<EnemyMovement>();
+        if (sceneEnemies.Length == 0)
+        {
+            return;
+        }
+        Transform closestEnemy = sceneEnemies[0].transform;
+
+        foreach (EnemyMovement testEnemy in sceneEnemies)
+        {
+            closestEnemy = getClosestEnemy(closestEnemy, testEnemy.transform);
+        }
+
+        targetEnemy = closestEnemy;
+    }
+
+    private Transform getClosestEnemy(Transform transformA, Transform transformB)
+    {
+        var distToA = Vector3.Distance(transform.position, transformA.position);
+        var distToB = Vector3.Distance(transform.position, transformB.position);
+
+        if (distToA < distToB)
+        {
+            return transformA;
+        }
+        return transformB;
+
     }
 
     private void ShootEnemy()
---
+++ TowerDefense/Assets/enemySpawning.cs
@@ -0,0 +1,33 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class enemySpawning : MonoBehaviour
+{
+    [SerializeField] float secBetweenSpawns = 5f;
+    [SerializeField] GameObject Enemy;
+
+    // Start is called before the first frame update
+    void Start()
+    {
+
+        StartCoroutine(Spawning(Enemy));
+    }
+
+    IEnumerator Spawning(GameObject Enemy)
+    {
+        while (true)
+        {
+
+            print("spawning");
+
+            Instantiate(Enemy);
+
+            yield return new WaitForSeconds(secBetweenSpawns);
+        }
+
+        // Update is called once per frame
+    }
+}
+        
+   
